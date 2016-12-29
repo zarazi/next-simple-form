88e5e1831b4d1ad0819aae6e4623dbf32f6f244e
zarazi <zarazi@hotmail.com>
2016-12-29 22:07:46 +0700

    Initial repo

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default ({}) => (
+  <div>
+    <Head>
+      <title>Simple Form</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <h3>Simple Form</h3>
+    <style jsx>{`
+      h3 {
+        font-weight: lighter;
+      }
+    `}</style>
+  </div>
+)
